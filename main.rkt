b64aee06fbcf188899bc6ece82d924cbe136832c
Ajay Vallurupalli <ajay.vallurupalli@gmail.com>
2025-03-18 11:09:36 -0400

    threaded the calculations so that the application wouldn't freeze while running, and created a main drawing thread to ensure proper coloring.

--- main.rkt
+++ main.rkt
@@ -1,6 +1,6 @@
-#lang racket/gui
+#lang racket/base
 
-(require math/matrix)
+(require math/matrix racket/gui)
 
 (define WIDTH 600)
 (define HEIGHT 400)
@@ -35,13 +35,12 @@
 (define matrix-top-left (new text-field%
                              [label ""]
                              [parent matrix-top]
-                             [init-value "1"]))
+                             [init-value "-2"]))
 
 (define matrix-top-right (new text-field%
                               [label ""]
                               [parent matrix-top]
-                              [init-value "0"]))
-
+                              [init-value "8"]))
 
 (define matrix-bottom (new horizontal-panel%
                            [parent matrix-panel]
@@ -51,14 +50,44 @@
 (define matrix-bottom-left (new text-field%
                                 [label ""]
                                 [parent matrix-bottom]
-                                [init-value "0"]))
+                                [init-value "-1"]))
 
 (define matrix-bottom-right (new text-field%
                                  [label ""]
                                  [parent matrix-bottom]
-                                 [init-value "1"]))
+                                 [init-value "2"]))
+
+
+(define vector-panel (new group-box-panel%
+                          [label "Vector"]
+                          [parent panel]
+                          [min-width 80]
+                          [min-height 80]
+                          [vert-margin 10]))
+
+(define vector-top-panel (new horizontal-panel%
+                              [parent vector-panel]
+                              [min-width 80]
+                              [min-height 40]))
+
+(define vector-top (new text-field%
+                        [label ""]
+                        [parent vector-top-panel]
+                        [init-value "50"]))
+
+(define vector-bottom-panel (new horizontal-panel%
+                                 [parent vector-panel]
+                                 [min-width 80]
+                                 [min-height 40]))
+
+(define vector-bottom (new text-field%
+                           [label ""]
+                           [parent vector-bottom-panel]
+                           [init-value "50"]))
 
 (define dc (send canvas get-dc))
+
+
 
 
 (define (draw-slope x y dx dy dc)
@@ -86,16 +115,13 @@
   (for-each (λ (p d) (draw-slope (first p) (second p) (first d) (second d) dc))
             pairs dpairs))
 
-(define a (matrix [[1 0] [0 1]]))
-
-(define (main)
-  (send frame show #t)
-  (send dc set-pen "black" 2 'solid)
-  (send dc draw-line (/ WIDTH 2) 0 (/ WIDTH 2) HEIGHT)
-  (send dc draw-line 0 (/ HEIGHT 2) WIDTH (/ HEIGHT 2))
-  (send dc set-pen "black" 1 'solid)
-  (send dc set-font (make-object font% 6 'swiss)))
-
+(define controls (new group-box-panel%
+                      [label "Controls"]
+                      [parent panel]
+                      [min-width 200]
+                      [min-height 80]
+                      [vert-margin 10]
+                      [alignment (list 'left 'center)]))
 
 (define (use-matrix _ __)
   (define tl (string->number (send matrix-top-left get-value)))
@@ -106,6 +132,8 @@
   
   (if (and tl tr bl br)
       (begin
+        (for-each kill-thread working-threads)
+        (set! working-threads (list))
         (main)
         (draw-slopes-from-matrix
          (matrix [[tl tr] [bl br]])
@@ -115,34 +143,110 @@
             (/ WIDTH 2)
             (/ HEIGHT 2))))
 
-(define vector-panel (new group-box-panel%
-                          [label "Vector"]
-                          [parent panel]
-                          [min-width 80]
-                          [min-height 80]
-                          [vert-margin 10]))
+(define matrix-button (new button%
+                           [label "Plot Matrix"]
+                           [parent controls]
+                           [callback use-matrix]))
 
-(define vector-top-panel (new horizontal-panel%
-                              [parent vector-panel]
-                              [min-width 80]
-                              [min-height 40]))
+(define (plot-vector _ __)
+  (define top (string->number (send vector-top get-value)))
+  (define bottom (string->number (send vector-bottom get-value)))
+  (define tl (string->number (send matrix-top-left get-value)))
+  (define tr (string->number (send matrix-top-right get-value)))
+  (define bl (string->number (send matrix-bottom-left get-value)))
+  (define br (string->number (send matrix-bottom-right get-value)))
 
-(define vector-top (new text-field%
-                        [label ""]
-                        [parent vector-top-panel]
-                        [init-value "0"]))
+  (define pen (make-object color%
+                (random 1 180)
+                (random 1 180)
+                (random 1 180)))
+  
+  (if (and top bottom tl tr bl br)
+      (begin
+        (set! working-threads
+              (append working-threads
+                      (list (thread (λ () (trajectory
+                                           (col-matrix [top bottom])
+                                           (matrix [[tl tr] [bl br]])
+                                           (send slider get-value)
+                                           (send slider get-value)
+                                           1
+                                           (send slider-2 get-value)
+                                           pen
+                                           dc)))
+                            (thread (λ () (trajectory
+                                           (col-matrix [top bottom])
+                                           (matrix [[tl tr] [bl br]])
+                                           (send slider get-value)
+                                           (send slider get-value)
+                                           -1
+                                           (send slider-2 get-value)
+                                           pen
+                                           dc)))))))
+      (send dc draw-text
+            "The numbers given are not numbers man"
+            (/ WIDTH 2)
+            (/ HEIGHT 2))))
 
-(define vector-bottom-panel (new horizontal-panel%
-                                 [parent vector-panel]
-                                 [min-width 80]
-                                 [min-height 40]))
+(define vector-button (new button%
+                           [label "Plot Vector Trajectory"]
+                           [parent controls]
+                           [callback plot-vector]))
 
-(define vector-bottom (new text-field%
-                           [label ""]
-                           [parent vector-bottom-panel]
-                           [init-value "0"]))
+(define slider (new slider%
+                    [label "Trajectory Steps"]
+                    [parent controls]
+                    [min-value 1]
+                    [max-value 5000]
+                    [init-value 2500]
+                    [style (list 'horizontal 'vertical-label)]))
 
-(define (trajectory vector matrix iterations total-iterations sign step dc)
+(define slider-2 (new slider%
+                      [label "Step Length (More is better)"]
+                      [parent controls]
+                      [min-value 1]
+                      [max-value 1000]
+                      [init-value 500]
+                      [style (list 'horizontal 'vertical-label)]))  
+
+(struct drawing (type data pen))
+
+(define (drawing-manager dc)
+  (define order (thread-receive))
+  (define data (drawing-data order))
+  (send dc set-pen (drawing-pen order))
+
+  (if (= drawing-type 'text)
+      (send dc draw-text
+            (first data) ;test
+            (second data);x
+            (third data))  ;y
+      (if (= drawing-type 'line)
+          (send dc draw-line
+                (first data)
+                (second data)
+                (third data)
+                (fourth data))
+          (void)))
+
+  (drawing-manager dc))
+  
+
+(define drawing-thread
+  (thread (λ () (drawing-manager dc))))
+
+(define working-threads (list))
+
+(define (main)
+  (send frame show #t)
+  (send dc set-pen "black" 2 'solid)
+  (send dc draw-line (/ WIDTH 2) 0 (/ WIDTH 2) HEIGHT)
+  (send dc draw-line 0 (/ HEIGHT 2) WIDTH (/ HEIGHT 2))
+  (send dc set-pen "black" 1 'solid)
+  (send dc set-font (make-object font% 12 'swiss)))
+
+(define (trajectory vector matrix iterations total-iterations sign step pen dc)
+  (send dc set-pen pen 2 'solid)
   (define next (matrix+ (matrix-map (λ (a) (/ (* a sign) step)) (matrix* matrix vector)) vector))
   (define next-pair (matrix->list next))
   (define vector-pair (matrix->list vector))
@@ -159,79 +263,7 @@
           (-  (/ HEIGHT 2) (second vector-pair))))
 
   (unless (= iterations 0)
-    (trajectory next matrix (sub1 iterations) total-iterations sign step dc)))
-
-(define (plot-vector _ __)
-  (define top (string->number (send vector-top get-value)))
-  (define bottom (string->number (send vector-bottom get-value)))
-  (define tl (string->number (send matrix-top-left get-value)))
-  (define tr (string->number (send matrix-top-right get-value)))
-  (define bl (string->number (send matrix-bottom-left get-value)))
-  (define br (string->number (send matrix-bottom-right get-value)))
-
-  (send dc set-pen
-        (make-object color%
-          (random 1 180)
-          (random 1 180)
-          (random 1 180))
-        2 'solid)
-  
-  (if (and top bottom tl tr bl br)
-      (begin
-        (trajectory
-         (col-matrix [top bottom])
-         (matrix [[tl tr] [bl br]])
-         (send slider get-value)
-         (send slider get-value)
-         1
-         (send slider-2 get-value)
-         dc)
-        (trajectory
-         (col-matrix [top bottom])
-         (matrix [[tl tr] [bl br]])
-         (send slider get-value)
-         (send slider get-value)
-         -1
-         (send slider-2 get-value)
-         dc))
-      (send dc draw-text
-            "The numbers given are not numbers man"
-            (/ WIDTH 2)
-            (/ HEIGHT 2))))
-
-(define controls (new group-box-panel%
-                      [label "Controls"]
-                      [parent panel]
-                      [min-width 200]
-                      [min-height 80]
-                      [vert-margin 10]
-                      [alignment (list 'left 'center)]))
-
-(define matrix-button (new button%
-                           [label "Plot Matrix"]
-                           [parent controls]
-                           [callback use-matrix]))
-
-(define vector-button (new button%
-                           [label "Plot Vector Trajectory"]
-                           [parent controls]
-                           [callback plot-vector]))
-
-(define slider (new slider%
-                    [label "Trajectory Steps"]
-                    [parent controls]
-                    [min-value 1]
-                    [max-value 5000]
-                    [init-value 100]
-                    [style (list 'horizontal 'vertical-label)]))
-
-(define slider-2 (new slider%
-                      [label "Step Length (over 1)"]
-                      [parent controls]
-                      [min-value 1]
-                      [max-value 1000]
-                      [init-value 1]
-                      [style (list 'horizontal 'vertical-label)]))  
+    (trajectory next matrix (sub1 iterations) total-iterations sign step pen dc)))
 
 (send dc set-font
       (make-object font%
